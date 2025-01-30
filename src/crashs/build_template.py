c8914b3429816efcbff9dde3f2402cbb1b46c480
Abi Heller-Wight <abi.heller@unmc.edu>
2025-01-30 14:01:50 -0600

    adding more debugger

--- src/crashs/build_template.py
+++ src/crashs/build_template.py
@@ -1,3 +1,4 @@
+import pdb
 import argparse
 import pathlib
 import json
@@ -134,7 +135,7 @@ def groupwise_similarity_registration_keops(tbs: TemplateBuildWorkspace, templat
             for i2, (k2, v2) in enumerate(md_ds.items()):
                 if k1 != k2:
                     print(f"Processing pair: {k1} and {k2}")
-
+                    pdb.set_trace()
                     # Define the symmetric loss for this pair
                     loss_ab = lossVarifoldSurfWithLabels(v1.ft, v2.vt, v2.ft, v1.lpt, v2.lpt, kernel)
                     loss_ba = lossVarifoldSurfWithLabels(v2.ft, v1.vt, v1.ft, v2.lpt, v1.lpt, kernel)
